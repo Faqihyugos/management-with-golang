0318641434af6fcf0ac0a5c7482bfd9fe54af7ef
Faqihyugos <faqihzero795@gmail.com>
2022-06-12 16:49:32 +0700

    add folder menu

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := "root:Secret@tcp(127.0.0.1:3306)/db_restaurant?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
